7591525b848ac94c5e96c8de391acb54878fc2c1
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-09-22 20:22:20 +0700

    Camera: don't display picture size unless supported
    Otherwise the option will force close when selected
    
    Change-Id: I2047c90ab08e7d7abcf11d817b5ddf106df2c732
    
    Conflicts:
    	src/com/android/camera/PhotoMenu.java

--- src/com/android/camera/PhotoMenu.java
+++ src/com/android/camera/PhotoMenu.java
@@ -170,46 +170,23 @@ public class PhotoMenu extends PieController
             }
         }
         // image size
-        item = makeItem(R.drawable.ic_imagesize);
         final ListPreference sizePref = group.findPreference(CameraSettings.KEY_PICTURE_SIZE);
-        item.setLabel(res.getString(R.string.pref_camera_picturesize_title).toUpperCase(locale));
-        item.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(PieItem item) {
-                ListPrefSettingPopup popup = (ListPrefSettingPopup) mActivity.getLayoutInflater().inflate(
-                        R.layout.list_pref_setting_popup, null, false);
-                popup.initialize(sizePref);
-                popup.setSettingChangedListener(PhotoMenu.this);
-                mUI.dismissPopup();
-                mPopup = popup;
-                mUI.showPopup(mPopup);
-            }
-        });
-        settings.addItem(item);
-        // jpeg quality
-        if (group.findPreference(CameraSettings.KEY_CAMERA_JPEG) != null) {
-            item = makeItem(R.drawable.ic_jpeg);
-            final ListPreference effectPref = group.findPreference(CameraSettings.KEY_CAMERA_JPEG);
-            item.setLabel(res.getString(R.string.pref_jpeg_title).toUpperCase(locale));
+        if (sizePref != null) {
+            item = makeItem(R.drawable.ic_imagesize);
+            item.setLabel(res.getString(R.string.pref_camera_picturesize_title).toUpperCase(locale));
             item.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(PieItem item) {
                     ListPrefSettingPopup popup = (ListPrefSettingPopup) mActivity.getLayoutInflater().inflate(
                             R.layout.list_pref_setting_popup, null, false);
-                    popup.initialize(effectPref);
+                    popup.initialize(sizePref);
                     popup.setSettingChangedListener(PhotoMenu.this);
                     mUI.dismissPopup();
                     mPopup = popup;
                     mUI.showPopup(mPopup);
                 }
             });
-            settings.addItem(item);
-        }
-        // true view
-        if (group.findPreference(CameraSettings.KEY_TRUE_VIEW) != null) {
-            item = makeSwitchItem(CameraSettings.KEY_TRUE_VIEW, true);
-            item.setLabel(res.getString(R.string.pref_true_view_label).toUpperCase(locale));
-            settings.addItem(item);
+            more.addItem(item);
         }
         // Storage location
         if (group.findPreference(CameraSettings.KEY_STORAGE) != null) {
@@ -273,5 +250,5 @@ public class PhotoMenu extends PieController
         }
         super.onSettingChanged(pref);
     }
-
 }
+
